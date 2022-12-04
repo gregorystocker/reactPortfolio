a29306c8795d21726d53c2f7580ec553fe59bfb3
gregorystocker <gstocker@asu.edu>
2022-12-03 21:54:25 -0800

    initial commit of react portfolio

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# Goals for this updated portfolio
+
+- [ ] Each project should show it's own 'page'
+- [ ] The top NavBar should be viewable on any page
+- [ ] Should include a form to send me an email
+- [ ] Should show my resume
+- [ ] Should provide some sort of animation on transitions and loading
+
+
--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Home from './screens/Home.js';
+//src/screens/Home.js
 function App() {
+  let num = "Hello Num";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          num is: {num}
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +21,9 @@ function App() {
         >
           Learn React
         </a>
+        <Home></Home>
       </header>
+
     </div>
   );
 }
