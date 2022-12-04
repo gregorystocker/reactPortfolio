782158b52f237063f0dd4a3f4511b99ad73b5f63
gregorystocker <gstocker@asu.edu>
2022-12-03 22:25:15 -0800

    Implementing useState successfully

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './screens/Home.js';
+import { MDCRipple } from '@material/ripple';
+import { useState } from 'react';
 //src/screens/Home.js
 function App() {
-  let num = "Hello Num";
+  const [num, setNum] = useState(0);
+  function sayHello() {
+    setNum(num + 1);
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button class="mdc-button" onClick={sayHello}>
+          Add one to num</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
           num is: {num}
         </p>
-
         <a
           className="App-link"
           href="https://reactjs.org"
