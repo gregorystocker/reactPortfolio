fea61a0e784e81a2cef52adbc861234918d9887d
gregorystocker <gstocker@asu.edu>
2023-05-03 18:27:51 -0700

    Added canvas, didn't need any library

--- src/screens/Projects.js
+++ src/screens/Projects.js
@@ -5,7 +5,17 @@ export default function Projects() {
     return (
         <div>
             Projects PlaceHolder
-        </div>
+            <canvas width={500} height={500} style={
+                {
+                    color: "blue",
+                    background: "#2E0A0A",
+                    borderRadius: "10%"
+                }
+
+            }
+            ></canvas>
+        </div >
+
 
     )
 }
